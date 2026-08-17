34d7eadec8cbd7257c05a42f9c648202f72f9473
Rafael <RAFADOMANGUE>
2026-08-17 11:39:37 -0300

    Iniciando o back em springboot, e atualizando para java 25 o pom.xml

---
+++ trabalhoFinal/src/main/java/com/web2/trabalhoFinal/TrabalhoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.web2.trabalhoFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalApplication.class, args);
+	}
+
+}
