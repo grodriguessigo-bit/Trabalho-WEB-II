1f43cc238322c7e79a472b4717766aee6e72823c
Calebe Carozzi <calebercarozzi@outlook.com>
2026-08-20 18:20:01 -0300

    feat: add first entity as a model

---
+++ trabalhoFinal/src/main/java/com/web2/trabalhoFinal/model/Category.java
@@ -0,0 +1,46 @@
+package com.web2.trabalhoFinal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private boolean ativo = true;
+
+    public Category() {
+    }
+
+    public Category(String nome) {
+        this.nome = nome;
+        this.ativo = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+}
---
+++ trabalhoFinal/src/main/java/com/web2/trabalhoFinal/model/enums/EstadoSolicitacao.java
@@ -0,0 +1,13 @@
+package com.web2.trabalhoFinal.model.enums;
+
+public enum EstadoSolicitacao {
+
+    ABERTA,
+    ORCADA,
+    REJEITADA,
+    APROVADA,
+    REDIRECIONADA,
+    ARRUMADA,
+    PAGA,
+    FINALIZADA
+}
