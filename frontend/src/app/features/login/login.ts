e78896caf8812cff8bb8eb5f5211e883b4a5e246
vinicius henrique fredrich <vinihfredi@gmail.com>
2026-08-27 23:05:18 -0300

    version 0.1  of the login page with a login class on login.ts and a few functionalities, such as a validation for the email format

--- frontend/src/app/features/login/login.ts
+++ frontend/src/app/features/login/login.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+
+export class Login {
+  private status: string = '';
+
+  fazerLogin(email: string, senha: string): void {
+    if (!email.includes('@')) {
+      this.status = 'E-mail inválido, não tem o "@" esperado'; //depois tem que arrumar pra um formato certo
+      return;
+    }
+
+    this.status = `Tentando login com e-mail "${email}" e senha "${senha}"`;
+  }
+
+  get mensagem(): string {
+    return this.status;
+  }
+}
