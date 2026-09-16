b92c314fbe5ddf6bc5678c352a1050afbef9fda8
Calebe Carozzi <calebercarozzi@outlook.com>
2026-09-16 18:32:25 -0300

    feat: add mock for testing status buttons

--- frontend/src/app/services/maintenance-request.service.ts
+++ frontend/src/app/services/maintenance-request.service.ts
@@ -13,10 +13,71 @@ export class MaintenanceRequestService {
   listAll(): MaintenanceRequest[] {
     const requests = localStorage.getItem(LS_KEY);
 
-    if (!requests) {
-      return [];
+    // if (!requests) {
+    //   return [];
+    // }
+
+    //tirar isso aqui de dentro do if depois e tirar o comentario do de cima
+    if(requests){
+      return JSON.parse(requests);
     }
-    return JSON.parse(requests);
+
+    return [
+      new MaintenanceRequest(
+        1,
+        1,
+        3,
+        'Notebook Dell',
+        'Não liga',
+        '2026-09-10T10:00:00',
+        RequestStatus.OPEN,
+        [],
+      ),
+
+      new MaintenanceRequest(
+        2,
+        1,
+        2,
+        'Tablet Samsung',
+        'Tela quebrada',
+        '2026-09-11T14:30:00',
+        RequestStatus.REJECTED,
+        [],
+      ),
+
+      new MaintenanceRequest(
+        3,
+        1,
+        4,
+        'Computador Gamer',
+        'Desliga sozinho',
+        '2026-09-12T09:15:00',
+        RequestStatus.APPROVED,
+        [],
+      ),
+
+      new MaintenanceRequest(
+        4,
+        1,
+        1,
+        'Celular Motorola',
+        'Não carrega',
+        '2026-09-13T17:40:00',
+        RequestStatus.FIXED,
+        [],
+      ),
+
+      new MaintenanceRequest(
+        5,
+        1,
+        3,
+        'Notebook Lenovo',
+        'Teclado falhando',
+        '2026-09-14T11:20:00',
+        RequestStatus.QUOTED,
+        [],
+      ),
+    ];
   }
 
   insert(request: MaintenanceRequest) {
