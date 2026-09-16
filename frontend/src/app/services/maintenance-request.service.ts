8903c8e2961e607e2c6338fa4a17f07a4f82290d
Calebe Carozzi <calebercarozzi@outlook.com>
2026-09-16 17:05:00 -0300

    fix: error in maintenanceService

--- frontend/src/app/services/maintenance-request.service.ts
+++ frontend/src/app/services/maintenance-request.service.ts
@@ -5,64 +5,53 @@ import { RequestStatus } from '../shared/enum/request-status.enum';
 const LS_KEY = 'maintenance-request';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class MaintenanceRequestService {
 
-  listAll(): MaintenanceRequest[]{
+  listAll(): MaintenanceRequest[] {
     const requests = localStorage.getItem(LS_KEY);
 
-  if(!requests){
-    return [];
-  }
-  return JSON.parse(requests);
+    if (!requests) {
+      return [];
+    }
+    return JSON.parse(requests);
   }
 
-  insert(request: MaintenanceRequest){
-    const requests = this.listAll()
+  insert(request: MaintenanceRequest) {
+    const requests = this.listAll();
 
     request.id = this.generateId(requests);
 
     requests.push(request);
 
     localStorage.setItem(LS_KEY, JSON.stringify(requests));
-
   }
 
-  findById(id: number): MaintenanceRequest | undefined{
-    return this.listAll().find(
-      request => request.id === id
-    );
+  findById(id: number): MaintenanceRequest | undefined {
+    return this.listAll().find((request) => request.id === id);
   }
 
-  findByClientId(clientId: number):MaintenanceRequest[]{
+  findByClientId(clientId: number): MaintenanceRequest[] {
     return this.listAll()
       .filter((request) => request.clientId === clientId)
       .sort(
-        (a, b) =>
-          new Date(a.requestDateTime).getTime() -
-          new Date(b.requestDateTime).getTime(),
+        (a, b) => new Date(a.requestDateTime).getTime() - new Date(b.requestDateTime).getTime(),
       );
   }
 
-  findOpenRequest(): MaintenanceRequest[]{
-    return this.listAll().filter(
-      request => request.status === RequestStatus.OPEN
-    );
+  findOpenRequest(): MaintenanceRequest[] {
+    return this.listAll().filter((request) => request.status === RequestStatus.OPEN);
   }
 
-  private generateId(requests: MaintenanceRequest[]):number{
+  private generateId(requests: MaintenanceRequest[]): number {
     if (requests.length === 0) {
       return 1;
     }
 
-    const highestId = Math.max(
-      ...requests.map(request => request.id)
-    );
+    const highestId = Math.max(...requests.map((request) => request.id));
 
     return highestId + 1;
   }
-
 }
 
