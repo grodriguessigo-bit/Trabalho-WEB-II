dacd42c3c7e4184bf8703b377aaae2698bcb3ab4
Rafael <RAFADOMANGUE>
2026-09-16 01:43:56 -0300

    feat: colocando a rota client/equipament-repair

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { RegisterClientComponent } from './components/client/register-client/register-client.component';
+import { ClientEquipamentRepairComponent } from './components/client/client-equipament-repair/client-equipament-repair.component';
 import { ClientHomeComponent } from './components/client/client-home/client-home.component';
 import { EmployeeHomeComponent } from './components/employee/employee-home/employee-home.component';
 
@@ -18,6 +19,10 @@ export const routes: Routes = [
   {
     path: 'client/register',
     component: RegisterClientComponent
+  },
+  {
+    path: 'client/equipament-repair',
+    component: ClientEquipamentRepairComponent
   },
   {
     path: 'client/home',
