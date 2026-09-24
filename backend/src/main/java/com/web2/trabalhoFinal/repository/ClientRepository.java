3abe796c0754b6c0e0601e4216b6c04e6a3cb2d9
Rafael <RAFADOMANGUE>
2026-09-24 14:24:43 -0300

    Feat: Implementando algumas validações da classe Client.Service, utilizar para validar o CPF a biblioteca stella, E LEMBREM TODOS DE UTILIZAR A CLASSE APIRESPONDE PARA PADRONIZAR AS RESPOSTAS

--- backend/src/main/java/com/web2/trabalhoFinal/repository/ClientRepository.java
+++ backend/src/main/java/com/web2/trabalhoFinal/repository/ClientRepository.java
@@ -2,6 +2,10 @@ package com.web2.trabalhoFinal.repository;
 
 import com.web2.trabalhoFinal.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
 }
--- backend/src/main/java/com/web2/trabalhoFinal/service/ClientService.java
+++ backend/src/main/java/com/web2/trabalhoFinal/service/ClientService.java
@@ -1,6 +1,34 @@
 package com.web2.trabalhoFinal.service;
+import com.web2.trabalhoFinal.dto.ApiResponse;
+import com.web2.trabalhoFinal.entities.Client;
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.caelum.stella.validation.InvalidStateException;
+import com.web2.trabalhoFinal.repository.ClientRepository;
+import org.springframework.stereotype.Service;
 
-
-
+@Service
 public class ClientService {
+
+    private ClientRepository clientRepository;
+
+    public ApiResponse<Client> validarCliente(Client client){
+
+        CPFValidator cpfValidator = new CPFValidator();
+
+        try
+        {
+            if (!cpfValidator.invalidMessagesFor(client.getCpf()).isEmpty())
+                return new ApiResponse<>(null, "CPF informado é inválido", false);
+            if(client.getPhone().isBlank())
+                return new ApiResponse<>(null, "Campo phone está vazio", false);
+
+            Client clientSalvo = clientRepository.save(client);
+            return new ApiResponse<>(clientSalvo, "Sucesso ao cadastrar cliente", true);
+        }
+
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
